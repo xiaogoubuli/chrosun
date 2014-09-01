9fe6d7f5857755114efa4fd675606483efd2a167
xiaogoubuli <xiaogoubuli@163.com>
2014-09-01 12:46:57 +0800

    bg

--- js/chrosun.js
+++ js/chrosun.js
@@ -1,4 +1,5 @@
 (function() {
+    chrome.browserAction.setIcon({path: 'images/online.png'});
     $.getJSON('http://int.dpool.sina.com.cn/iplookup/iplookup.php?format=json', function(data) {
         $('#city').text('地址：' + data.country + ' - ' + data.province + ' - ' + data.city);
     });
